e5076d1c18d17519fe1b175ed5ff50effa3d2916
cheon13 <cheon.cv@gmail.com>
2024-03-03 14:09:12 -0500

    Ajout de gh = github CLI

--- hotes/serveur/configuration.nix
+++ hotes/serveur/configuration.nix
@@ -176,6 +176,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   neovim
   git
+  gh #github CLI pour faciliter l'authentification avec github.
   gnupg
   pinentry
   pinentry-curses
