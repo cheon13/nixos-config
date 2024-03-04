848d80c431c81b886dfc93d7ae2b30f201889228
Christian Héon <cheon.cv@gmail.com>
2024-03-04 17:33:43 -0500

    correction de l input pour wsl

--- hotes/wsl-portable/configuration.nix
+++ hotes/wsl-portable/configuration.nix
@@ -9,7 +9,7 @@
 
 { imports = [
     # include NixOS-WSL modules
-    #<nixos-wsl/modules>
+    <nixos-wsl/modules>
     ];
 
   wsl.enable = true; wsl.defaultUser = "cheon"; wsl.wslConf.network.generateHosts = false;
