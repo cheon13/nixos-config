e9d7e26b14ad54efc0dc5c48c3f2bbb5eb6aa2e2
cheon13 <cheon.cv@gmail.com>
2024-03-06 16:41:50 -0500

    Ajout configuration du clavier pour hyprland

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -76,6 +76,18 @@
           )
           10)
       );
+  
+    input = {
+        kb_layout = "ca";
+        #kb_variant =
+        #kb_model =
+        #kb_options = 
+        #kb_rules =
+    
+        follow_mouse = "1";
+        sensitivity = "0"; # -1.0 - 1.0, 0 means no modification.
+    }
+    
   };
 
 }
