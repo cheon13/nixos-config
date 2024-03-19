df5197208d7056eb0b4505909a2638ec4fd662e3
cheon13 <cheon.cv@gmail.com>
2024-03-18 21:51:01 -0400

    Ajout d une configuration pour construire un image Iso de NixOX

--- hotes/isoImage/configuration.nix
+++ hotes/isoImage/configuration.nix
@@ -13,7 +13,7 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
 
   # Ajout manuel au fichiers /etc/hosts
   networking.extraHosts = ''
