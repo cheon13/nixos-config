964757da4b785b42bae77f68e17637ce3e8e8995
cheon13 <cheon.cv@gmail.com>
2024-06-29 14:10:53 -0400

    ajustement de keybindings pour sway

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -26,6 +26,29 @@
         "${modifier2}+Shift+e" = "exec ~/.config/sway/scripts/power-menu.sh";
         # "${modifier}+Shift+e" = "exec swaymsg exit";
         # "${modifier}+Shift+e" = "exec swaynag -t warning -m 'Voulez-vous vraiment quitter ?' -b 'Yes, exit sway' 'swaymsg exit'";
+	
+        "${modifier2}+b" = "splith";
+        "${modifier2}+v" = "splitv";
+        "${modifier2}+f" = "fullscreen toggle";
+        "${modifier2}+a" = "focus parent";
+
+        "${modifier2}+s" = "layout stacking";
+        "${modifier2}+w" = "layout tabbed";
+        "${modifier2}+e" = "layout toggle split";
+
+        "${modifier2}+Shift+space" = "floating toggle";
+        "${modifier2}+space" = "focus mode_toggle";
+
+        "${modifier2}+f1" = "workspace number 1";
+        "${modifier2}+f2" = "workspace number 2";
+        "${modifier2}+f3" = "workspace number 3";
+        "${modifier2}+f4" = "workspace number 4";
+        "${modifier2}+f5" = "workspace number 5";
+        "${modifier2}+f6" = "workspace number 6";
+        "${modifier2}+f7" = "workspace number 7";
+        "${modifier2}+f8" = "workspace number 8";
+        "${modifier2}+f9" = "workspace number 9";
+        "${modifier2}+f0" = "workspace number 10";
       };
       terminal = "kitty";
       menu = "wofi --show drun";
