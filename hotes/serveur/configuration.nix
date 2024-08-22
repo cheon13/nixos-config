b621f853656660d77ae8440890c0c117114e163d
cheon13 <cheon.cv@gmail.com>
2024-08-21 21:17:21 -0400

    Essai de DWM de NixOS

--- hotes/serveur/configuration.nix
+++ hotes/serveur/configuration.nix
@@ -15,7 +15,7 @@
   
   services.xserver.enable = true;
   services.xserver.displayManager.startx.enable = true;
-  # services.xserver.windowManager.dwm.enable = true;
+  services.xserver.windowManager.dwm.enable = true;
 
   programs.hyprland.enable = true; 
   programs.sway.enable = true; 
