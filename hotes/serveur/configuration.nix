0e93b4e17d994cbda6a2e0f2e86a71ef1a3209dd
cheon13 <cheon.cv@gmail.com>
2024-08-22 12:23:08 -0400

    Ajout des sources pour DWM 6.5

--- hotes/serveur/configuration.nix
+++ hotes/serveur/configuration.nix
@@ -16,6 +16,9 @@
   services.xserver.enable = true;
   services.xserver.displayManager.startx.enable = true;
   services.xserver.windowManager.dwm.enable = true;
+  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
+    src = ../../modules/nixos/dwm;
+  };
   services.xserver.xkb = {
     layout = "ca";
     # variant = "fr";
