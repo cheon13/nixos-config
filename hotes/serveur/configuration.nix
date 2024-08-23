6dcb29932a37289fcaff0ab6831ef0ebd43295c5
cheon13 <cheon.cv@gmail.com>
2024-08-22 20:11:06 -0400

    Configuration de la source de st

--- hotes/serveur/configuration.nix
+++ hotes/serveur/configuration.nix
@@ -146,11 +146,16 @@
   rclone
   xdg-utils
   #xorg.xinit
-  st
+  st.package = pkgs.st.overrideAttrs
+  {
+    src = ../../modules/nixos/st;
+  };
+
   dmenu
   slstatus
   xclip
   nitrogen
+  dwl
   nodePackages.nodejs  # pour utiliser le plugin coc.nvim
   ltex-ls              # pour utiliser coc-ltex
   ];
@@ -195,7 +200,7 @@
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
+  # this value at the release version of the firspath/to/dwm/source/treet install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
