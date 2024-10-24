d34614253c374d12036cebca1ef6e6eeb18e66ff
cheon13 <cheon.cv@gmail.com>
2024-10-24 10:44:46 -0400

    Mise à jour de la configuration hyprland

--- hotes/portable/configuration.nix
+++ hotes/portable/configuration.nix
@@ -24,14 +24,7 @@
     # variant = "fr";
     #options = "caps:swapescape";
   };
-  #programs.hyprland.enable = true; 
-  programs.hyprland = {
-    enable = true;
-    # set the flake package
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  };
+  programs.hyprland.enable = true; 
   programs.sway.enable = true; 
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
