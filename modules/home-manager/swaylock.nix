4c3a3104b0ba7c24eee089c4e712a4c2d1007240
cheon13 <cheon.cv@gmail.com>
2024-10-29 14:02:01 -0400

    Ajouter la config de swaylock dans homo manager

---
+++ modules/home-manager/swaylock.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  # Configuration de swaylock
+  programs.swaylock = {
+	enable = true;
+	settings = {
+	  image = "$HOME/Images/Wallpapers/swayWP";
+	  scaling = "fill";
+	  font = "UbuntuMono";
+	  font-size = 20;
+	  indicator-radius = 115;
+          line-color = "#3b4252";
+          text-color = "#d8dee9";
+          inside-color = "#2e344098";
+          inside-ver-color = "#5e81ac";
+          line-ver-color = "#5e81ac";
+          ring-ver-color = "#5e81ac98";
+          ring-color = "#4c566a";
+          key-hl-color = "#5e81ac";
+          separator-color = "#4c566a";
+          layout-text-color = "#eceff4";
+          line-wrong-color = "#d08770";
+	};
+  };
+}
