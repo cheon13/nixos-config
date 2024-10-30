ff15bb06f2f82f7b221254172201e54917d24b76
cheon13 <cheon.cv@gmail.com>
2024-10-30 19:07:44 -0400

    Ajout de .xprofile dans home.nix de portable

--- hotes/portable/home.nix
+++ hotes/portable/home.nix
@@ -98,6 +98,21 @@
         nk = "NVIM_APPNAME='nvim-kickstart' nvim";
       };
   };
+  
+  xsession = {
+      enable = true;
+      profileExtra = ''
+# Fichier de configuration pour startx
+#
+# Pour le fond d'écran
+nitrogen --restore &
+# Pour donner le status dans la barre de DWM
+slstatus &
+# La ligne suivant n'est pas nécessaire parce que intercept-tools s'en occupe pour la Console, Wayland et X11
+# setxkbmap -option caps:swapescape
+# exec dwm  # cette ligne est nécessaire si les fichiers est .xinitrc, mais pas pour .xprofile
+      '';
+  };
 
   programs.kitty = {
       enable = true;
