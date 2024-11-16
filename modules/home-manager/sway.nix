c0db8623a3027672d3283b3b2cb8007903491619
cheon13 <cheon.cv@gmail.com>
2024-11-16 11:31:17 -0500

    Ajout de la touche XF86AudioMicMute dans sway.nix

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -117,6 +117,7 @@
         "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5% && $sink_volume";
         "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5% && $sink_volume";
         "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle && pactl get-sink-mute @DEFAULT_SINK@ | sed -En '/no/ s/.*/$($sink_volume)/p; /yes/ s/.*/0/p'";
+        "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
       };
       terminal = "kitty";
       menu = "wofi --show drun";
