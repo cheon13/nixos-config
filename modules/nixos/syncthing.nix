dcf104289fe14e3328ef72353623afdbda41ddbd
cheon13 <cheon.cv@gmail.com>
2024-12-01 20:42:48 -0500

    Réparation de la configuration synthing - ajout de serveur

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -15,6 +15,7 @@
       settings.devices = {
         #"portable" = { id = "WOSHMB7-N5YIJTM-VLAPPHS-HUQS2QO-CEO67WO-MF4LJIW-4HGO563-LPEVCAZ"; };
         "portable" = { id = "2H6R2EE-GUGFVTC-EJ2PJV6-EGU3F3J-J32ZAGY-VG3V5Z6-CLRI2G2-T7GIIA5"; };
+        "serveur" = { id = "ZXIDF7N-OYKKYUC-XV4MJEB-TT5G7TL-Q32AGSN-PE3GQJ6-3QYZQIJ-3646AQU"; };
         "phone" = { id = "WNJ5NHU-FCEJGLJ-TBESUFK-XPMKGBN-H225KSH-JZB7XB5-FGAQUWH-4WXIVQF"; };
         "DESKTOP-UEM9CA3" = { id = "FWKDNXG-UIMQA6F-FTBAGSP-VKD34DH-DFGQDWO-ZOPEKOJ-KV54A6J-IVU44AM"; };
       };
@@ -23,7 +24,7 @@
           id = "mbh3e-b0zp2";
           path = "/home/cheon/Documents/Cerveau"; 
           #devices = ["phone" "DESKTOP-UEM9CA3"]; 
-          devices = [ "portable" "phone" "DESKTOP-UEM9CA3"]; 
+          devices = [ "portable" "serveur" "phone" "DESKTOP-UEM9CA3"]; 
           versioning = { 
             type = "simple"; 
             params = { 
