6de0877f99d9a395bb94b755d7b11308c0ed473b
cheon13 <cheon.cv@gmail.com>
2024-12-05 23:40:32 -0500

    Ajout du plugins mini.files à init.lua

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -116,6 +116,9 @@ map ('n', '<Down>', '<Plug>VimwikiNextLink')
 -----Pour insérer la date
 map ('n', '<Leader>d', ':r! date \\+\\%A" "\\%d" "\\%B" "\\%Y| sed "s/\\b\\(.\\)/\\u\\1/"<cr>')
 
+-----Pour activer ouvrir mini.files
+map ('n', '<Leader>f', function() require('mini.files').open() end) -- Permet de chercher un fichier 
+
 ----------------
 ---- 4 Plugins 
 ----------------
