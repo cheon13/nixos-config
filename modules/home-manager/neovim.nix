c2d1343355d7202f8db4562fd5bcce8edc26cac2
cheon13 <cheon.cv@gmail.com>
2025-01-01 20:05:44 -0500

    Ménage dans neovim.nix et ajout du plugin rocks-nvim pour avoir luarocks

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -127,101 +127,19 @@ map ('n', '<Leader>f', function() require('mini.files').open() end) -- Permet de
 require 'lualine'.setup()
 ----Activation du plugin mini.files
 require 'mini.files'.setup()
+
 	'';
-#    extraLuaConfig = 
-#    	''
-#	  -- Mon fichier de configuration inspiré de celui dépôt Github numToStr/dotfiles 
-#          -- Dernier changement: 30 juin 2023
-#          
-#          ---------------
-#          -- 1 Settings
-#          ---------------
-#          local g = vim.g
-#          local o = vim.o
-#          local opt = vim.opt
-#          
-#          g.mapleader = " "
-#          g.mapleader = " "
-#          opt.number = true 
-#          opt.relativenumber = true
-#          
-#          ---- pour utiliser y et p pour le copier coller avec le clipboard
-#          o.clipboard = 'unnamedplus'
-#          
-#          ---- Configuration du correcteur orthographique
-#          vim.opt.spelllang = 'en_us,fr'
-#          vim.opt.spell = true
-#          
-#          ---- Configuration pour que VimWiki utilise Markdown
-#          g.vimwiki_list = {{path = '~/Documents/Cerveau', syntax = 'markdown', ext = '.md'}}
-#          
-#          ---- Configuration pour ajuster le contraste de limelight
-#          g.limelight_conceal_ctermfg = 'DarkGray'
-#          
-#          ---- Configuration pour le colorscheme
-#          vim.o.background = "dark" -- or "dark" or "light" for light mode
-#          vim.cmd([[colorscheme gruvbox]])
-#          
-#          -- Set highlight on search
-#          vim.o.hlsearch = false
-#          
-#          -- Save undo history
-#          vim.o.undofile = true
-#          
-#          -- Case-insensitive searching UNLESS \C or capital in search
-#          vim.o.ignorecase = true
-#          vim.o.smartcase = true
-#          
-#          --------------
-#          -- 3 Autocmd
-#          --------------
-#          local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
-#          local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
-#          
-#          --Pour exécuter rapidement un script python dans un terminal
-#          autocmd('Filetype', { 
-#            pattern = 'python', 
-#            command = 'nnoremap <buffer> <F7> :w<CR>:vs<CR>:ter python "%"<CR>'
-#            })
-#          
-#          ----Pour exécuter rapidement afficher un fichier markdown dans un terminal
-#          autocmd('Filetype', { 
-#            pattern = 'markdown', 
-#            command = 'nnoremap <buffer> <F7> :w<CR>:!pandoc "%" -o prttmp.pdf --resource-path="%:p:h" && zathura prttmp.pdf && rm prttmp.pdf<CR>'
-#            })
-#          
-#          ---- Template pour le diary de Vimwiki
-#          autocmd('BufNewFile', {
-#            pattern = '/home/cheon/Documents/Cerveau/diary/[0-9]*.md',
-#            command = [[silent 0r !echo "\# `date +\%Y-\%m-\%d`"]] 
-#            -- command = 'silent 0r !echo # `date +%Y-%m-%d`' 
-#            })
-#          
-#          ------------------
-#          -- 3 Keybindings
-#          ------------------
-#          local function map(m, k, v)
-#              vim.keymap.set(m, k, v, { silent = true })
-#          end
-#          
-#          ---- Pour faciliter la navigation entre les splits
-#          map('n', '<C-J>', '<C-W><C-J>')
-#          map('n', '<C-K>', '<C-W><C-K>')
-#          map('n', '<C-L>', '<C-W><C-L>')
-#          map('n', '<C-H>', '<C-W><C-H>')
-#          map('n', '<leader>z', ':TZAtaraxis<CR>')
-#          
-#          ----pour utiliser la commande point avec visualautocmd VimEnter * 
-#          map('v', '.', ':norm.<CR>')
-#          ----pour utiliser fzf avec vimwiki
-#          --------Pour insérer le nom de fichier complet
-#          map ('i', '<c-x><c-f>', '<plug>(fzf-complete-path)')
-#          --------Pour ouvrir un fichier de Vimwiki
-#          map ('n', '<Leader>wp', ':Files ~/Documents/Cerveau/<CR>')
-#          ----Activation du plugin lualine
-#	  require 'lualine'.setup()
-#	'';
-      plugins = with pkgs.vimPlugins; [ zen-mode-nvim twilight-nvim  vim-pencil vimwiki gruvbox lualine-nvim fzf-lua nvim-web-devicons mini-files ];
-#      plugins = with pkgs.vimPlugins; [ true-zen-nvim twilight-nvim  vim-pencil vimwiki gruvbox lualine-nvim fzf-vim ];
+      plugins = with pkgs.vimPlugins; [ 
+         zen-mode-nvim 
+         twilight-nvim  
+         vim-pencil 
+         vimwiki 
+         gruvbox 
+         lualine-nvim 
+         fzf-lua 
+         nvim-web-devicons 
+         mini-files
+         rocks-nvim
+         ];
   };
 }
