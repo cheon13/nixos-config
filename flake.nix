79099fd92712eedbd122dfdfc0c54a1fbc57b67a
cheon13 <cheon.cv@gmail.com>
2025-01-08 19:54:59 -0500

    Ajout du module nixvim au homemanager

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
        modules = [
         ./hotes/portable/configuration.nix
         stylix.nixosModules.stylix
-        nixvim.homeManagerModules.nixvim
+        #nixvim.homeManagerModules.nixvim
         #nixvim.nixosModules.nixvim
         home-manager.nixosModules.home-manager
           {
