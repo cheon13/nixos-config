5f1f8a6ae9d5005e94e44afa11056565a8f37fe7
cheon13 <cheon.cv@gmail.com>
2025-02-05 19:39:57 -0500

    Changement de la base de donnée de Nextcloud de sqlite à mysql

--- modules/nixos/nextcloud/default.nix
+++ modules/nixos/nextcloud/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 {
-  environment.etc."nextcloud-admin-pass".text = "Ceciestuntestnextcloud";
+  environment.etc."nextcloud-admin-pass".text = "9=UiSDr?Rcl>CK~JB=4B?4A]9";
   
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud30;
     hostName = "10.0.0.200";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
-    config.dbtype = "sqlite";
+    config.dbtype = "mysql";
   };
   
   networking.firewall.allowedTCPPorts = [ 80 443 ];
