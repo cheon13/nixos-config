0a152b0c51f22b1dc1726659ee0be25858331f3c
cheon13 <cheon.cv@gmail.com>
2025-04-16 16:41:43 -0400

    ajout de l'extension pop-shell à gnome

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -5,5 +5,8 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true; 
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [ gnomeExtensions.pop-shell ];
+
 }
 
