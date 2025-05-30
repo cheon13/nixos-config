bc83e1c11c03d71ee934bbc5c93e75f682a4467e
cheon13 <cheon.cv@gmail.com>
2025-05-30 19:48:40 -0400

    Ajustement de la configuration de kanata

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -80,13 +80,13 @@
     enable = true;
     keyboards = {
       internalKeyboard = {
-        devices = [
-          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-          "/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
-          "/dev/input/by-path/pci-0000:00:14.0-usb-0:9:1.1-event-kbd"
-          "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:9:1.1-event-kbd"
-          "/dev/input/by-id/usb-Logitech_USB_Receiver-if01-event-kbd"
-        ];
+        # devices = [
+        #   "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        #   "/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
+        #   "/dev/input/by-path/pci-0000:00:14.0-usb-0:9:1.1-event-kbd"
+        #   "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:9:1.1-event-kbd"
+        #   "/dev/input/by-id/usb-Logitech_USB_Receiver-if01-event-kbd"
+        # ];
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
           (defsrc
