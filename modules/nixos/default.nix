3c307ab37282d194a7293b0b127c36e3919fa839
cheon13 <cheon.cv@gmail.com>
2025-05-31 16:41:32 -0400

    Ajustement de Kanata

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -90,7 +90,7 @@
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
           (defsrc
-           esc caps l t i u
+           esc caps l r t i
            lmet
           )
 
