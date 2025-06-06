b7c908a01347c698d91966b13e5edbd8adddefa1
cheon13 <cheon.cv@gmail.com>
2025-06-05 21:15:25 -0400

    Corriger les options de GDM et Gnome

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -3,8 +3,8 @@
 {
   # Installation du display manager gnome
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true; 
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true; 
+  services.desktopManager.gnome.enable = true;
 
   environment.systemPackages = with pkgs; [ gnomeExtensions.pop-shell ];
 
