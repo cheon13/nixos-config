d7378065cd67b800aa376f3fc5c70fdd90d1e1fe
cheon13 <cheon.cv@gmail.com>
2025-06-27 18:11:26 -0400

    Ajout d'un répertoire partagé synthing pour la musique de Dominique

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -13,18 +13,27 @@
       overrideDevices = true;
       overrideFolders = true;
       settings.devices = {
-        #"portable" = { id = "WOSHMB7-N5YIJTM-VLAPPHS-HUQS2QO-CEO67WO-MF4LJIW-4HGO563-LPEVCAZ"; };
+        "portable-dominique" = { id = "ATG2335-BJBH5IY-3QZWKG2-GX6HH63-OLAC3WY-7DCT6F5-J3JKK7R-I23XKA5"; };
         "portable" = { id = "2H6R2EE-GUGFVTC-EJ2PJV6-EGU3F3J-J32ZAGY-VG3V5Z6-CLRI2G2-T7GIIA5"; };
         "serveur" = { id = "ZXIDF7N-OYKKYUC-XV4MJEB-TT5G7TL-Q32AGSN-PE3GQJ6-3QYZQIJ-3646AQU"; };
         "phone" = { id = "WNJ5NHU-FCEJGLJ-TBESUFK-XPMKGBN-H225KSH-JZB7XB5-FGAQUWH-4WXIVQF"; };
-        "DESKTOP-UEM9CA3" = { id = "FWKDNXG-UIMQA6F-FTBAGSP-VKD34DH-DFGQDWO-ZOPEKOJ-KV54A6J-IVU44AM"; };
       };
       settings.folders = {
         "Cerveau" = { 
           id = "mbh3e-b0zp2";
           path = "/home/cheon/Documents/Cerveau"; 
-          #devices = ["phone" "DESKTOP-UEM9CA3"]; 
-          devices = [ "portable" "serveur" "phone" "DESKTOP-UEM9CA3"]; 
+          devices = [ "portable" "serveur" "phone"]; 
+          versioning = { 
+            type = "simple"; 
+            params = { 
+              keep = "10"; 
+            }; 
+          }; 
+        };
+        "musique-dominique" = { 
+          id = "hnkxk-d5dgu";
+          path = "/var/Navidrome-music/Dominique/"; 
+          devices = [ "portable-dominique" "serveur"]; 
           versioning = { 
             type = "simple"; 
             params = { 
