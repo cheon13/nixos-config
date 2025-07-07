3463f3bc8b82fe2fdcf6281dd4c34ee5a192181c
cheon13 <cheon.cv@gmail.com>
2025-07-07 13:39:32 -0400

    Adaptation des scripts reseau matugen et waybar

--- modules/home-manager/waybar/scripts/reseau.sh
+++ modules/home-manager/waybar/scripts/reseau.sh
@@ -1,7 +1,11 @@
-#/bin/sh
+#!/usr/bin/env bash
+
+source ~/.config/waybar/matugen/bemenu-colors.sh
+
+bemenu-run --prompt "Lancer" -i -W 0.3 -c -l 20 --fn JetBrainsMono 14 --fb $background --ff $foreground --nb $background --nf $foreground --tb $primary --hb $background --tf $on_primary --hf $primary  --af $foreground --ab $background -B 0 --bdr $foreground
 
 # Script pour un menu de gestion réseau avec waybar
-export BEMENU_OPTS="-i -W 0.3 -c -l 20 --fn JetBrainsMono 14 --fb '#282828' --ff '#ebdbb2' --nb '#282828' --nf '#ebdbb2' --tb '#282828' --hb '#282828' --tf '#fb4934' --hf '#fabd2f'  --af '#ebdbb2' --ab '#282828' -B 2 --bdr '#ebdbb2'"
+export BEMENU_OPTS="-i -W 0.3 -c -l 20 --fn JetBrainsMono 14 --fb $background --ff '#ebdbb2' --nb '#282828' --nf '#ebdbb2' --tb '#282828' --hb '#282828' --tf '#fb4934' --hf '#fabd2f'  --af '#ebdbb2' --ab '#282828' -B 2 --bdr '#ebdbb2'"
 
 choix=$(printf "activer mode avion\ndesactiver mode avion\nconnecter a un reseau wifi\nnetwork manager applet" | bemenu --prompt "Réseau")
 
