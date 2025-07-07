33c1eceab94a69d0de8cba8846eaba943f841ae8
cheon13 <cheon.cv@gmail.com>
2025-07-07 13:52:06 -0400

    Adaptation des scripts reseau matugen et waybar

--- modules/home-manager/waybar/scripts/connect-wifi.sh
+++ modules/home-manager/waybar/scripts/connect-wifi.sh
@@ -2,6 +2,8 @@
 
 # Script pour un menu pour se connecter à un réseau wifi
 
+source ~/.config/waybar/matugen/bemenu-colors.sh
+
 export BEMENU_OPTS="-i -W 0.3 -c -l 20 --fn JetBrainsMono 14 --fb $background --ff $foreground --nb $background --nf $foreground --tb $primary --hb $background --tf $on_primary --hf $primary  --af $foreground --ab $background -B 0 --bdr $foreground"
 
 ssid=$(nmcli -f SSID device wifi | bemenu --prompt "Choisir le réseau" |sed 's/[[:blank:]]*$//' )
