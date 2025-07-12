8f729a72fe1ed47f847ecaebe51f901b2fe086ed
cheon13 <cheon.cv@gmail.com>
2025-07-12 00:26:11 -0400

    Ajout de openssh pour phone

--- hotes/phone/nix-on-droid.nix
+++ hotes/phone/nix-on-droid.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
   # Simply install just the packages
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
