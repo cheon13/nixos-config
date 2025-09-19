b0965dfa81a33f8d795ba6f273d9462ef7be2527
cheon13 <cheon.cv@gmail.com>
2025-09-19 19:04:59 -0400

    Changement de la police de swaylock

--- modules/home-manager/swaylock.nix
+++ modules/home-manager/swaylock.nix
@@ -8,7 +8,7 @@
 	  indicator-caps-lock = true;
 	  image = "$HOME/Images/Wallpapers/riverWP";
 	  scaling = "fill";
-	  font = "UbuntuMono";
+	  font = "Adwaita Mono";
 	  font-size = 20;
 	  indicator-radius = 115;
           line-color = "#3b4252";
