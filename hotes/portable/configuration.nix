b838e55c62d9cd4a813e01016f27eb4362361f02
cheon13 <cheon.cv@gmail.com>
2026-02-11 18:45:35 -0500

    Ajout de flatpak

--- hotes/portable/configuration.nix
+++ hotes/portable/configuration.nix
@@ -60,31 +60,10 @@
     };
   };
 
-  # Optimisation de la batterie pour le laptop
-  # semble entrer en conflit avec un autre service :
-  #    services.power-profiles-daemon
-  #    est-ce lié à gnome ?
-  #services.tlp = {
-  #      enable = true;
-  #      settings = {
-  #        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-  #
-  #        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-  #        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-  #
-  #        CPU_MIN_PERF_ON_AC = 0;
-  #        CPU_MAX_PERF_ON_AC = 100;
-  #        CPU_MIN_PERF_ON_BAT = 0;
-  #        CPU_MAX_PERF_ON_BAT = 20;
-  #
-  #       #Optional helps save long term battery health
-  #       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-  #       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-  #
-  #      };
-  #};
+  
+  # service pour installer des logiciel flatpak
+  services.flatpak.enable = true;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; 
 
 }
