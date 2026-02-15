1d06c320b05b0bf83f59f760e5c003b3c75642c4
cheon13 <cheon.cv@gmail.com>
2026-02-15 16:22:05 -0500

    Re-Test de navidrome unstable

--- modules/nixos/navidrome.nix
+++ modules/nixos/navidrome.nix
@@ -1,13 +1,14 @@
 { config, pkgs, pkgs-unstable, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    pkgs-unstable.navidrome
-  ];
+#  environment.systemPackages = with pkgs; [
+#    pkgs-unstable.navidrome
+#  ];
 
   # Service de music streaming
   services.navidrome = {
     enable = true;
+    package = pkgs-unstable.navidrome;
     openFirewall = true;
     settings = {
       Address = "10.0.0.200";
